aca466cee0642fbf350997cb0f9b5ddbccca3677
Sweats <alrightcoolio@hotmail.com>
2014-02-03 17:12:06 -0500

    Create Chapter 1 Exercise 2 PrintNameWithFriends.cpp

---
+++ Chapter 1 Exercise 2 PrintNameWithFriends.cpp
@@ -0,0 +1,14 @@
+/*Write a program that displays multiple lines of text onto the screen, each one displaying the name of one of your friends*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Sweats" << endl;
+    cout << "Friend 1" << endl;
+    cout << "Friend 2" << endl;
+    cout << "Friend 3" << endl;
+    cout << "Friend 4" << endl;
+}
