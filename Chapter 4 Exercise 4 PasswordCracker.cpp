218edff9c1cf9f62850c3467cd0ca1405da0e679
Sweats <alrightcoolio@hotmail.com>
2014-02-05 10:56:23 -0500

    Create Chapter 4 Exercise 4 PasswordCracker.cpp

---
+++ Chapter 4 Exercise 4 PasswordCracker.cpp
@@ -0,0 +1,32 @@
+/* Write a password prompt that gives a user only a certain number of password entry attempts— 
+so that the user cannot easily write a password cracker. */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string password;
+    cout << "Enter the password. You only have 5 tries." << endl;
+    cin >> password;
+
+    if(password == "sweats")
+    {
+        cout << "You did it!" << endl;
+        return 0;
+    }
+    else
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            cout << i << " tries used." << endl;
+            cin >> password;
+
+            if(i == 5)
+            {
+                cout << "All 5 tries were already used. Program terminating..." << endl;
+                return 0;
+            }
+        }
+    }
