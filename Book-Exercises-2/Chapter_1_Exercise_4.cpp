f72285bc7ec9f73cf7d49d8dd52d120c8b82feb8
Sweats <alrightcoolio@hotmail.com>
2014-04-19 00:40:24 -0400

    Exercise Four

---
+++ Book-Exercises-2/Chapter_1_Exercise_4.cpp
@@ -0,0 +1,19 @@
+/* Write a program that asks the user to enter his or her age. The program then should
+display the age in months:
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int years, months;
+
+    cout << "Enter your age to convert into months" << endl;
+    cin >> years;
+
+    months = years * 12;
+
+    cout << "Your age in months is: " << months << "." << endl;
+}
