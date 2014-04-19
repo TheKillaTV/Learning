89893a676ed71e24873e0b1b99c2982963708f6f
Sweats <alrightcoolio@hotmail.com>
2014-04-19 00:24:08 -0400

    First Exercise

---
+++ Book-Exercises-2/Chapter_1_Exercise_1.cpp
@@ -0,0 +1,19 @@
+/*
+Write a C++ program that displays your name and address (or if you value your
+privacy, a fictitious name and address).
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string sweats = "Sweats", address = "1337 Lelcopter";
+
+    cout << "Name is " << sweats << endl;
+    cout << endl;
+    cout << "Address is " << address << endl;
+    cout << endl;
+}
