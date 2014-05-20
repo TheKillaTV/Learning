b69e4cb7955e0761578976a179c0590f51e79433
Sweats <alrightcoolio@hotmail.com>
2014-05-20 15:46:36 -0400

    Create Chapter_6_Exercise_01.cpp

---
+++ Book-Exercises-2/Chapter_6_Exercise_01.cpp
@@ -0,0 +1,38 @@
+/* Write a program that reads keyboard input to the @ symbol and that echoes the input except for digits,
+converting each uppercase character to lowercase, and vice versa. (Don't forget the cctype family).
+*/
+
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+
+int main()
+{
+    char input;
+
+    cout << "Enter a word: ";
+    cin.get(input);
+
+    while(input != '@')
+    {
+        if(islower(input))
+        {
+            input = toupper(input);
+            cout << input << " is now uppercase." << endl;
+            break;
+        }
+        else if(isupper(input))
+        {
+            input = tolower(input);
+            cout << input << " is now lowercase." << endl;
+            break;
+        }
+        else if(input >= 0 || input <= 0)
+        {
+            cout << "Don't enter a digit" << endl;
+            return 0;
+        }
+    }
+}
