be4344e434097cd332453d42dab7e077f0bc422f
Sweats <alrightcoolio@hotmail.com>
2014-12-29 01:20:48 -0500

    Update Pancake Glutton.cpp

--- Custom Exercises I do for fun/Pancake Glutton.cpp
+++ Custom Exercises I do for fun/Pancake Glutton.cpp
@@ -1,13 +1,11 @@
-/*
-
-Requires:
+/*Requires:
 variables, data types, and numerical operators
-basic input/output
-logic (if statements, switch statements)
-loops (for, while, do-while)
+basic input / output
+logic(if statements, switch statements)
+loops(for, while, do - while)
 arrays
 
-Write a program that asks the user to enter the number of pancakes eaten for breakfast by 10 different people (Person 1, Person 2, ..., Person 10)
+Write a program that asks the user to enter the number of pancakes eaten for breakfast by 10 different people(Person 1, Person 2, ..., Person 10)
 Once the data has been entered the program must analyze the data and output which person ate the most pancakes for breakfast.
 
 ★ Modify the program so that it also outputs which person ate the least number of pancakes for breakfast.
@@ -15,46 +13,56 @@ Once the data has been entered the program must analyze the data and output whic
 ★★★★ Modify the program so that it outputs a list in order of number of pancakes eaten of all 10 people.
 i.e.
 Person 4: ate 10 pancakes
-Person 3: ate 7 pancakes
-Person 8: ate 4 pancakes
+Person 3 : ate 7 pancakes
+Person 8 : ate 4 pancakes
 ...
-Person 5: ate 0 pancakes
-
+Person 5 : ate 0 pancakes
 */
 
 #include <iostream>
-#include <algorithm>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+const int size = 11;
+
+void InputArray(int arr[], int size);
+void OutputArray(const int arr[], int size);
 
 int main()
 {
-    int people[10] = {0};
-    int pancakes[10] = {0};
-    cout << "Enter the number of pancakes eaten for breakfast by 10 different people" << endl;
+	int Pancakes[size] = { 0 };
+	InputArray(Pancakes, size);
+	OutputArray(Pancakes, size);
+}
 
-    for(int i  = 0; i < 10; i++)
-    {
-        cout << "Enter person number: ";
-        cin >> people[i];
-        cout << "How many pancakes did person: " << people[i] << " eat? ";
-        cin >> pancakes[i];
-    }
+void InputArray(int arr[], int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		cout << "Enter number of pancakes eaten for person " << i << "." << endl;
+		cin >> arr[i];
+	}
+}
 
-    cout << endl << endl;
-
-    for(int j = 0; j < 10; j++)
-    {
-        cout << "Person: " << people[j] << " ate " << pancakes[j] << " pancakes." << endl;
-    }
-    cout << endl;
-
-    sort(pancakes, pancakes + 10);
-
-    cout << "SORTED NUMBERS:" << endl << endl;
-
-    for(int i = 0; i < 10; i++)
-    {
-        cout << "Person: " << people[i] << " ate " << pancakes[i] << " pancakes" << endl;
-    }
+void OutputArray(const int arr[], int size)
+{
+	int temp, temp2 = 0, most = 0, least = 0;
+	for (int i = 1; i < size; i++)
+	{
+		if (arr[i] < temp)
+		{
+			temp = arr[i];
+			least = i;
+		}
+		else if (arr[i] > temp2)
+		{
+			temp2 = arr[i];
+			most = i;
+		}
+		cout << "Person " << i << ": ate " << arr[i] << " pancakes." << endl;
+	}
+	cout << "Person " << most << " ate the most amount of pancakes (" << temp2 << ")" << endl;
+	cout << "Person " << least << " ate the least amount of pancakes (" << temp << ")" << endl;
 }
