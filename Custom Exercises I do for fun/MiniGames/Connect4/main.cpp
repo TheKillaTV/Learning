dfd529603f5e4d5aa285c5a553d9ff1ae07d7bef
Sweats <alrightcoolio@hotmail.com>
2015-05-13 17:18:26 -0400

    Create main.cpp

---
+++ Custom Exercises I do for fun/MiniGames/Connect4/main.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "Connect4.h"
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	Connect4 Test;
+
+	while (true)
+	{
+		Test.PrintBoard();
+		Test.PlayerOneTurn();
+		Test.PutPlayerOnePiece();
+		Test.ScanBoard();
+
+		if (Test.Winner >= 100) // Big number for p1 incase it detectes two connect 4's at once.
+		{
+			Test.PrintBoard();
+			cout << "Player 1 wins! " << endl;
+			break;
+		}
+
+		Test.PrintBoard();
+		Test.PlayerTwoTurn();
+		Test.ScanBoard();
+
+		if (Test.Winner >= 1)
+		{
+			Test.PrintBoard();
+			cout << "Player 2 wins! " << endl;
+			break;
+		}
+	}
+}
