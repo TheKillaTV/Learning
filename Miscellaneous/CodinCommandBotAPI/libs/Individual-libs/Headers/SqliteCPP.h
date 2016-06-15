6414e9e9bb40322a09d94870b3aba9bf1fbd2d76
Sweats <alrightcoolio@hotmail.com>
2016-06-15 16:48:50 -0400

    Create SqliteCPP.h

---
+++ Miscellaneous/CodinCommandBotAPI/libs/Individual-libs/Headers/SqliteCPP.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// A small wrapper class for sqlite3 specifically for the kik bot . Will make future kik bot projects easier.
+
+#include "Global.h"
+#include <sqlite3.h>
+
+#ifndef SQLITE3CPP_H
+#define SQLITE3CPP_H
+
+class Sqlite3CPP
+{
+public:
+	Sqlite3CPP();
+	~Sqlite3CPP();
+	bool Open(const std::string & Filename);
+	bool Query(const std::string & Query);
+	bool Step();
+	void PrintLastError() const;
+	std::string GetColumnText(int x) const;
+	
+
+private:
+	sqlite3 * m_db;
+	sqlite3_stmt * m_statement;
+	std::string m_ErrorMessage;
+	int m_ErrorCode;
+
+};
+
+#endif
+
