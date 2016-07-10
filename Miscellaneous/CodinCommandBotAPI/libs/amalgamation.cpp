965fa262e37fd848f584d2776e0dc4d30e5c25a3
Sweats <alrightcoolio@hotmail.com>
2016-07-10 19:13:15 -0400

    Update amalgamation.cpp

--- Miscellaneous/CodinCommandBotAPI/libs/amalgamation.cpp
+++ Miscellaneous/CodinCommandBotAPI/libs/amalgamation.cpp
@@ -311,7 +311,7 @@ void Clipboard::PrintLastError() const
 
 #endif
 
-Sqlite3CPP::Sqlite3CPP() : m_ErrorCode(0)
+Sqlite3CPP::Sqlite3CPP() : m_ErrorCode(0), m_IsOpen(false)
 {
 
 }
@@ -319,22 +319,36 @@ Sqlite3CPP::Sqlite3CPP() : m_ErrorCode(0)
 
 Sqlite3CPP::~Sqlite3CPP()
 {
-	sqlite3_finalize(m_statement);
-	sqlite3_close(m_db);
+	if (m_IsOpen)
+	{
+		sqlite3_finalize(m_statement);
+		sqlite3_close(m_db);
+		m_IsOpen = false;
+	}
 }
 
 bool Sqlite3CPP::Open(const std::string & Filename)
 {
-	if (sqlite3_open(Filename.c_str(), &m_db) == SQLITE_OK)
+	if (!m_IsOpen)
 	{
-		return true;
+		if (sqlite3_open(Filename.c_str(), &m_db) == SQLITE_OK)
+		{
+			m_IsOpen = true;
+			return true;
+		}
+
+		else
+		{
+			m_ErrorMessage = sqlite3_errmsg(m_db);
+			m_ErrorCode = sqlite3_errcode(m_db);
+			sqlite3_close(m_db);
+			return false;
+		}
 	}
 
 	else
 	{
-		m_ErrorMessage = sqlite3_errmsg(m_db);
-		m_ErrorCode = sqlite3_errcode(m_db);
-		sqlite3_close(m_db);
+		std::cout << "Call to  bool Sqlite3CPP::Open(const std::string & Filename) failed. Reason: Database is already open!" << std::endl;
 		return false;
 	}
 }
@@ -347,15 +361,23 @@ void Sqlite3CPP::PrintLastError() const
 
 bool Sqlite3CPP::Query(const std::string & Query)
 {
-	if (sqlite3_prepare_v2(m_db, Query.c_str(), -1, &m_statement, NULL) == SQLITE_OK)
+	if (m_IsOpen)
 	{
-		return true;
+		if (sqlite3_prepare_v2(m_db, Query.c_str(), -1, &m_statement, NULL) == SQLITE_OK)
+		{
+			return true;
+		}
+
+		else
+		{
+			m_ErrorMessage = sqlite3_errmsg(m_db);
+			m_ErrorCode = sqlite3_errcode(m_db);
+		}
 	}
 
 	else
 	{
-		m_ErrorMessage = sqlite3_errmsg(m_db);
-		m_ErrorCode = sqlite3_errcode(m_db);
+		std::cout << "Call to bool Sqlite3CPP::Query(const std::string & Query) failed, Reason: Database isn't open!" << std::endl;
 	}
 
 	return false;
@@ -363,15 +385,23 @@ bool Sqlite3CPP::Query(const std::string & Query)
 
 bool Sqlite3CPP::Step()
 {
-	if (sqlite3_step(m_statement) == SQLITE_ROW)
+	if (m_IsOpen)
 	{
-		return true;
+		if (sqlite3_step(m_statement) == SQLITE_ROW)
+		{
+			return true;
+		}
+
+		else
+		{
+			m_ErrorMessage = sqlite3_errmsg(m_db);
+			m_ErrorCode = sqlite3_errcode(m_db);
+		}
 	}
 
 	else
 	{
-		m_ErrorMessage = sqlite3_errmsg(m_db);
-		m_ErrorCode = sqlite3_errcode(m_db);
+		std::cout << "Call to bool Sqlite3CPP::Step() failed. Reason: Database isn't open!" << std::endl;
 	}
 
 	return false;
@@ -382,6 +412,14 @@ bool Sqlite3CPP::Step()
 // x = 16 for isuser
 std::string Sqlite3CPP::GetColumnText(int x) const
 {
-	// An extremly hackish way to get the first column. Will improve this.
-	return (const char*)(sqlite3_column_text(m_statement, (sqlite3_column_count(m_statement) - sqlite3_column_count(m_statement)) + x));
+	if (m_IsOpen)
+	{
+		// An extremly hackish way to get the first column. Will improve this.
+		return (const char*)(sqlite3_column_text(m_statement, (sqlite3_column_count(m_statement) - sqlite3_column_count(m_statement)) + x));
+	}
+
+	else
+	{
+		return "NULL";
+	}
 }
